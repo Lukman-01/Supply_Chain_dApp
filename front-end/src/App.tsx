828728bc71963bb002f3fc6fa10dda82e4823653
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-06-18 11:57:21 +0100

    clean

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import Services from './components/Services';
-// import Home from './components/Home';
 import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
     <div className="flex-grow">
       <Navbar />
-      {/* <Home /> */}
-      {/* <Services /> */}
       <Footer />
     </div>
   );
